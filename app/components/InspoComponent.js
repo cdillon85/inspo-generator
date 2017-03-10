941a23531d6b5479d9d8b5c374853bb37c2f1cd8
Christine Dillon <christine.dillon@gmail.com>
2017-03-10 18:47:48 -0500

    Built basic functionality and styling for inspo component.

---
+++ app/components/InspoComponent.js
@@ -0,0 +1,47 @@
+import React from 'react'
+
+const InspoComponent = (props) => {
+
+	// const inspoQuote = {id:1, text:"You are never alone or helpless. The force that guides the stars guides you too.", author:"Shrii Shrii Anandamurti", moreInfoURL:"https://en.wikipedia.org/wiki/Prabhat_Ranjan_Sarkar"}
+	const inspoBlurb = {id:1, text:"If you want to radically change your life, strengthen your faith that we live in an abundant, benevolent Universe by being grateful for everything you've already manifested and all the good that's hurlting toward you. Be grateful that you have the power to manifest any reality you desire and then leap into the void to go get it. If you want to radically change your life, strengthen your faith that we live in an abundant, benevolent Universe by being grateful for everything you've already manifested and all the good that's hurlting toward you. Be grateful that you h. If you want to radically change your life, strengthen your faith that we live in an abundant, benevolent Universe by being grateful for everything you've already manifested and all the good that's hurlting toward you. Be grateful that you h.", author:"Jen Sincero", from:"You Are A Badass", moreInfoURL:"https://www.amazon.com/You-Are-Badass-Doubting-Greatness/dp/0762447699"}
+	
+// if (inspoQuote){
+// 	return (
+// 	<div>
+// 		<div className="headline-container">
+// 			<h1 className="headline">How about this?</h1>
+// 		</div>
+// 		<div className="inspo-container">
+// 		<div className="inspo-quote">
+// 			<div className="inspo-quote">"{inspoQuote.text}"</div>
+// 			<div className="inspo-author">~{inspoQuote.author}</div>
+// 			<div className="inspo-btn">Want to learn more?</div>
+// 			<div className="inspo-btn">Start Over</div>
+// 		</div>
+// 		</div>
+// 	</div>
+
+// 	)
+// else if (inspoBlurb) {
+	return (
+		<div>
+			<div className="headline-container">
+				<h1 className="headline">How about this?</h1>
+			</div>
+			<div className="inspo-container">
+			<div className="inspo-blurb">
+				<div>"{inspoBlurb.text}"</div>
+				<div className="inspo-author-blurb">~{inspoBlurb.author}</div>
+			</div>	
+				<div className="inspo-btn">Want to read more?</div>
+				<div className="inspo-btn">Start Over</div>
+			</div>
+		</div>
+
+		)
+// }
+
+
+}
+
+export default InspoComponent
