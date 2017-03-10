592aa443811ed5998223cd3e1b8de63c05a1d5f8
Christine Dillon <christine.dillon@gmail.com>
2017-03-10 16:19:28 -0500

    updated time component.

---
+++ app/components/TimeComponent.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const TimeComponent = (props) => {
+
+	return (
+	<div>
+		<div className="headline-container">
+			<h1 className="headline">How much time do you have?</h1>
+			<div className="single-btn-container">
+			<div className="single-btn">Less than 5 Minutes</div>
+			</div>
+			<br/>
+			<div className="single-btn-container">
+			<div className="single-btn">More than 5 Minutes</div>
+			</div>
+		</div>
+	</div>
+
+	)
+}
+
+export default TimeComponent
