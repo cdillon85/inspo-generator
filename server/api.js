8e33098704ed6207cb1c81d863c69b6835317092
Christine Dillon <christine.dillon@gmail.com>
2017-03-11 15:55:43 -0500

    Removed unecessary server files and updated index for server.

--- server/api.js
+++ server/api.js
@@ -3,10 +3,10 @@
 require('APP/db')
 const api = module.exports = require('express').Router() // eslint-disable-line new-cap
 
-api
-  .get('/heartbeat', (req, res) => res.send({ok: true}))
-  .use('/auth', require('./auth'))
-  .use('/users', require('./users'))
+// api
+  // .get('/heartbeat', (req, res) => res.send({ok: true}))
+  // .use('/auth', require('./auth'))
+  // .use('/users', require('./users'))
 
 // No routes matched? 404.
 api.use((req, res) => res.status(404).end())
