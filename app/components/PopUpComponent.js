d3f4e1f510cf576dcc90775d753d0ea9b8dbbf4d
Christine Dillon <christine.dillon@gmail.com>
2017-03-21 19:58:51 -0400

    adjusted styling for pop-up component.

--- app/components/PopUpComponent.js
+++ app/components/PopUpComponent.js
@@ -3,11 +3,13 @@ import React from 'react'
 const PopUpComponent = (props) => {
 const onClickHandler = props.onClick
 	return (
-		<div className="pop-up">
-		Did you know that you can highlight text and share it with friends? Try it out!
-		<div className="pop-up-btn-container">
-		<div className="pop-up-btn" onClick={() => onClickHandler()} >Got It!</div>
-		</div>
+		<div className="pop-up-container">
+			<div className="pop-up">
+			Did you know that you can highlight text and share it with friends? Try it out!
+			<div className="pop-up-btn-container">
+				<div className="pop-up-btn" onClick={() => onClickHandler()} >Got It!</div>
+				</div>
+			</div>
 		</div>
 		)
 }
